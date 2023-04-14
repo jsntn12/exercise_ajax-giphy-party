659792d8ca92f01d5d4821cee3d19635790c9128
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-04-13 21:39:45 -0400

    feat: User can enter the search term and the result is added to the below div

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 $('#searchGiphy').on('click', function (evt) {
-	alert('pressed');
 	evt.preventDefault();
+	let searchTerm = $('input').val();
+	getGiphy(searchTerm);
+
+	$('input').val('');
 });
 
 $('#removeImages').on('click', function (evt) {
-	alert('Remove');
 	evt.preventDefault();
 });
+
+async function getGiphy(keyword) {
+	let giphy = await axios.get(
+		`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+	);
+	addGiphy(giphy.data.data[0].images.original.url);
+}
+
+function addGiphy(results) {
+	console.log(results);
+	let newImg = $('<img>', { src: results, class: 'giphy' });
+	$('.allGiphyImages').append(newImg);
+}
